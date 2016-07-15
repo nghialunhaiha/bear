667ce3ba8cda4d94e8ff7d2d2e502623d9a7763a
nghia.pham <nghia.pham@ntq-solution.com.vn>
2016-07-15 12:04:28 +0700

    report html

---
+++ src/test/java/com/damienfremont/blog/RunBDDTest.java
@@ -0,0 +1,15 @@
+package com.damienfremont.blog;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+// @CucumberOptions(strict = false, features = "features", format = { "pretty", "html:target/site/cucumber-pretty",
+// "json:target/cucumber.json" }, tags = { "~@ignore" })
+
+@CucumberOptions(strict = false, features = "features", format = { "pretty", "json:target/cucumber.json" }, tags = {
+        "~@ignore" })
+public class RunBDDTest {
+
+}
